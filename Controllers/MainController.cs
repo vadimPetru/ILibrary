bfa40a76b0edcf2b05953ee00acdc9da949975a5
Vadim <6ddd14@gmail.com>
2023-01-13 18:51:50 +0300

    Add Pictures for main page and Create menu for the app

---
+++ Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ILibrary.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult MainContent()
+        {
+            return View();
+        }
+
+    }
+}
